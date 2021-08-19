8a532e3dbdc2dd41bf0cfd36d5e1a5a8f02d09be
Alfredo <fredogm05@gmail.com>
2021-08-19 11:40:41 -0400

    implementacion de los css y de la plantilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { WrapperComponent } from './shared/wrapper/wrapper.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { PagesComponent } from './pages/pages/pages.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    WrapperComponent,
+    DashboardComponent,
+    ChartsComponent,
+    PagesComponent,
   ],
   imports: [
     BrowserModule,
